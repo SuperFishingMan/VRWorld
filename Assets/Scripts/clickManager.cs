8d1ec6dd39c5dc26ec0035ef07dba76e745693b7
Carl David <ukunity3d@gmail.com>
2016-10-15 04:42:46 +0100

    Commit
    Imported Google VR

---
+++ Assets/Scripts/clickManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PointerLook ()
+    {
+
+        Debug.Log("Pointer made contact with item");
+    }
+}
---
+++ Assets/vrLookWalk.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vrLookWalk : MonoBehaviour {
+    public Transform vrCamera;
+
+    public float toggleAngle = 30.00f;
+
+    public float speed = 3.0f;
+
+    public bool moveForward;
+
+    private CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+
+        characterController = GetComponent<CharacterController>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.00f)
+        {
+            moveForward = true;
+
+        }
+        else
+        {
+            moveForward = false;
+        }
+
+        if (moveForward)
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
+            characterController.SimpleMove(forward * speed);
+        }
+	}
+}
